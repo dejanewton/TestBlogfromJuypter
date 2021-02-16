884f1da06737763b6f832a2888f57c516022af41
dejanewton <77369934+dejanewton@users.noreply.github.com>
2021-02-16 11:41:49 -0800

    Add files via upload

---
+++ Sample_Practice_Blog.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ![Random Unsplash Image](https://source.unsplash.com/random)
+
+# In[ ]:
+
+
+
+
+
+# > This is a blockquote.
+# > 
+# > This is the second paragraph in the blockquote.
+# >
+# > ## This is an H2 in a blockquote
+# 
+
+# In[ ]:
+
+
+
+
